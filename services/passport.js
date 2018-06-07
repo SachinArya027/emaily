40fe00b00ae4b97c6ec3384c5eb77edc1bc0ed61
SachinArya027 <sachin.arya027@gmail.com>
2018-06-07 09:35:19 +0530

    Added Billing and client server

--- services/passport.js
+++ services/passport.js
@@ -11,7 +11,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((id, done) => {
     User.findById(id)
-        .then(user  => {
+        .then(user => {
             done(null, user);
         });
 });
@@ -23,19 +23,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if(existingUser) {
-                        //we already have a user
-                        done(null, existingUser);
-                    } else {
-                        new User({
-                            googleId: profile.id
-                        }).save()
-                        .then(user => done(null, user));
-                    }
-                })
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({
+                googleId: profile.id
+            });
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
         }
     )
 );
