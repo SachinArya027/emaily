b27d6831dece16688d07fb673903fe1aba3064f1
SachinArya027 <sachin.arya027@gmail.com>
2018-07-03 07:37:19 +0530

    Fix for NO vote and langing page change

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center'}}>
-            <h1>Emaily</h1>
-            Collect feedback form your users
+            <h4>Welcome to Emaily!!!</h4>
+            Here you can collect feedback from your users.
         </div>
     );
 };
